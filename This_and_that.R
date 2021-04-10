20ab7e71db64e19e45217c7885c54c24f711dc4c
Christina Bogner <christina.bogner@uni-bayreuth.de>
2021-04-10 13:57:35 +0200

    Initial commit

---
+++ This_and_that.R
@@ -0,0 +1,4 @@
+# build book
+
+bookdown::render_book("index.Rmd", "bookdown::bs4_book")
+bookdown::render_book("index.Rmd", "bookdown::pdf_book")
---
+++ helper_functions.R
@@ -0,0 +1,31 @@
+# Load libraries
+library(fontawesome)
+library(kableExtra)
+library(tidyverse)
+
+knitr::knit_hooks$set(document = function(x) {
+  sub('\\usepackage{graphicx, color}',
+      '\\usepackage{graphicx,xcolor}', x, fixed = TRUE)
+}) 
+
+
+
+# These are different helper functions for the book
+
+colorize <- function(x, color) {
+  if (knitr::is_latex_output()) {
+    sprintf("\\textcolor{%s}{%s}", color, x)
+  } else if (knitr::is_html_output()) {
+    sprintf("<span style='color: %s;'>%s</span>", color, 
+            x)
+  } else x
+}
+
+file.name <- function(x) {
+  if (knitr::is_latex_output()) {
+    sprintf("\\texttt{%s}", x)
+  } else if (knitr::is_html_output()) {
+    sprintf("<span style='font-family: sans-serif; font-size: medium'>%s</span>",
+            x)
+  } else x
+}
